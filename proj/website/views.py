facff7d9b6e038e2735993173caf4b584d3cc2a5
Gabriel dos Anjos <cunha.gabriel@gmail.com>
2020-02-05 16:19:13 -0300

    Seção 2 - Aula 12

--- proj/website/views.py
+++ proj/website/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def hello_blog(request):
+    lista = [
+        'Django', 'Python', 'Html', 'Git',
+        'Banco de Dados', 'Linux', 'Nginx', 'Uwsgi',
+        'Systemctl'
+    ]
+    data = {'name' : 'Curso de Django 3', 'lista_tecnologias': lista}
+    return render(request, 'index.html', data)
