24c627b7802ecf73e1c42b2d7ee04695bdd4b0e0
Gabriel dos Anjos <cunha.gabriel@gmail.com>
2020-02-10 16:10:11 -0300

    Commit 10-02

--- proj/website/admin.py
+++ proj/website/admin.py
@@ -3,4 +3,9 @@ from .models import Post
 
 # Register your models here.
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'subtitle', 'full_name']
+    search_fields = ['title', 'subtitle']
+    fields = ('title', 'subtitle')
+
+admin.site.register(Post, PostAdmin)
--- proj/website/models.py
+++ proj/website/models.py
@@ -6,3 +6,12 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     subtitle = models.CharField(max_length=200)
     content = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def full_name(self):
+        return self.title + self.subtitle
+
+    full_name.admi
+    
