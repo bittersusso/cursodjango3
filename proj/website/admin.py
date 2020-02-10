6c2a50af9f27c15f8cd9e8817b633e1f10476c42
Gabriel dos Anjos <cunha.gabriel@gmail.com>
2020-02-10 17:51:47 -0300

    10-02 -> Ultimo Push do Dia

--- proj/website/admin.py
+++ proj/website/admin.py
@@ -4,8 +4,14 @@ from .models import Post
 # Register your models here.
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'subtitle', 'full_name']
+    list_display = [
+        'title', 'subtitle', 'full_name',
+         'categories', 'deleted'
+    ]
     search_fields = ['title', 'subtitle']
-    fields = ('title', 'subtitle')
+    #fields = ('title', 'subtitle')
+
+    def get_queryset(self, request):
+        return Post.objects.filter(deleted=False)
 
 admin.site.register(Post, PostAdmin)
--- proj/website/models.py
+++ proj/website/models.py
@@ -2,10 +2,21 @@ from django.db import models
 
 # Create your models here.
 
+class Categorias(models.TextChoices):
+    TECH = 'TC', 'Tecnologia'
+    CR = 'CR', 'Curiosidades'
+    GR = 'GR', 'Geral'
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     subtitle = models.CharField(max_length=200)
     content = models.TextField()
+    categories = models.CharField(
+        max_length=2,
+        choices=Categorias.choices,
+        default=Categorias.GR,
+    )
+    deleted = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
@@ -13,5 +24,5 @@ class Post(models.Model):
     def full_name(self):
         return self.title + self.subtitle
 
-    full_name.admi
+    full_name.admin_order_field = 'title'
     
